b635c6eb0f3b76039d25a7fde2d9c0f3fcc0d9d0
jstockwin <jstockwin@gmail.com>
2016-04-22 11:35:00 -0700

    Fix email js

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -12,7 +12,7 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var to = $("input#to").val()
+            var to = $("select#to").val()
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
