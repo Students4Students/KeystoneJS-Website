4b101609603a77276b8b53a4ce21409fcee7eb11
jstockwin <jstockwin@gmail.com>
2016-04-22 11:11:33 -0700

    Fix mixed content blocking emails

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://deployment.jakestockwin.co.uk/contact_me.php",
+                url: "https://deployment.jakestockwin.co.uk/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
