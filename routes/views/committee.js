b38ca54535d8aee38e33f7ea93187b796203c88b
jstockwin <jstockwin@gmail.com>
2016-05-09 17:08:37 +0100

    Fix committe and trustee pages sometime being empty

--- routes/views/committee.js
+++ routes/views/committee.js
@@ -1,24 +1,24 @@
 var keystone = require('keystone');
 var CommitteeMember = keystone.list('CommitteeMember');
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// Set locals
 	locals.section = 'about';
 	locals.subsection = 'committee';
-	
+
 	locals.members = [];
 	view.on('init', function(next) {
 		CommitteeMember.model.find().sort('displayOrder').exec(function(err, results) {
 			locals.members = results;
-		});		
-		next();		
+			next(err);
+		});
 	});
-	
-	
+
+
 	// Render the view
 	view.render('committee');
-	
+
 };
--- routes/views/trustees.js
+++ routes/views/trustees.js
@@ -1,24 +1,24 @@
 var keystone = require('keystone');
 var Trustee = keystone.list('Trustee');
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// Set locals
 	locals.section = 'about';
 	locals.subsection = 'trustees';
-	
+
 	locals.trustees = [];
 	view.on('init', function(next) {
 		Trustee.model.find().sort('displayOrder').exec(function(err, results) {
 			locals.trustees = results;
-		});		
-		next();		
+			next(err);
+		});
 	});
-	
-	
+
+
 	// Render the view
 	view.render('trustees');
-	
+
 };
