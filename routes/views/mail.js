cc6fbd7b2fee4ca16df64863a234b2829bc704e8
jstockwin <jstockwin@gmail.com>
2016-07-23 18:39:07 +0100

    Improve email script. Restrict use to S4S site.

--- routes/views/mail.js
+++ routes/views/mail.js
@@ -15,36 +15,40 @@ var config = {
 var transporter = nodemailer.createTransport(config);
 
 exports = module.exports = function (req, res) {
-	var view = new keystone.View(req, res);
-	view.on('post', function (next) {
 
-		var message = 'You have recieved a new message from your website contact form.<br><br>'
-		+ 'Here are the details:<br><br>'
-		+ 'Name: ' + req.body.name + '<br>'
-		+ 'Phone Number: ' + req.body.phone + '<br>'
-		+ 'Message: <br><br>' + req.body.message.replace('\n', '<br>');
+	var address = keystone.get('host') + ':' + keystone.get('port');
+	console.log(address);
+	console.log(req.headers.origin);
+	if (req.headers.origin.indexOf(address) === -1) {
+		// Don't let other people send emails using this server.
+		console.log('Banned');
+		res.statusCode = 403;
+		res.end();
+	}
 
-		var mailOptions = {
-			from: '"jakestockwin.co.uk" <noreply@jakestockwin.co.uk>', // sender address
-			to: req.body.to, // list of receivers
-			subject: 'S4S Website Contact Form', // Subject line
-			html: message, // plaintext body
-		};
+	var message = 'You have recieved a new message from your website contact form.<br><br>'
+	+ 'Here are the details:<br><br>'
+	+ 'Name: ' + req.body.name + '<br>'
+	+ 'Phone Number: ' + req.body.phone + '<br>'
+	+ 'Message: <br><br>' + req.body.message.replace('\n', '<br>');
 
-		transporter.sendMail(mailOptions, function (err, info) {
-			if (err) {
-				console.log(err);
-				res.statusCode = 500;
-				next(err);
-			} else {
-				console.log('Message sent: ' + info.response);
-				next();
-			}
+	var mailOptions = {
+		from: '"jakestockwin.co.uk" <noreply@jakestockwin.co.uk>', // sender address
+		to: req.body.to, // list of receivers
+		subject: 'S4S Website Contact Form', // Subject line
+		html: message, // plaintext body
+	};
 
-		});
+	transporter.sendMail(mailOptions, function (err, info) {
+		if (err) {
+			console.log(err);
+			res.statusCode = 500;
+		} else {
+			res.statusCode = 200;
+			console.log('Message sent: ' + info.response);
+		}
+		res.end();
 
 	});
-
-	view.render('contact');
 
 };
